23d5bd217404557c4e6fafffd4dee0bd2c36a8b5
Artur-JS <info.arahmanov@gmail.com>
2023-03-27 21:42:49 +0500

    Headers added

--- httpd/htdocs/index.php
+++ httpd/htdocs/index.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
 include_once('config.php');
 
 function countData(mysqli $mysqli): int{
